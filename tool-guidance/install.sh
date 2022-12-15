eb967cad3efd4249d586bbe005eafba9d010d28f
Roland Stens <roland.stens@gmail.com>
2022-12-15 11:00:17 -0800

    Added readme + updated install.sh to copy in the readme for keycloak

--- tool-guidance/install.sh
+++ tool-guidance/install.sh
@@ -88,6 +88,7 @@ then
   if [[ $keycloak == 'y' ]]
   then
     curl 'https://raw.githubusercontent.com/bcgov/automated-testing/main/tool-guidance/library/keycloak/commands.js' >> $testpath/cypress/support/commands.$code
+    curl 'https://raw.githubusercontent.com/bcgov/automated-testing/main/tool-guidance/library/keycloak/README.md' >> $testpath/cypress/e2e/examples/keycloak-example/README.md
     curl 'https://raw.githubusercontent.com/bcgov/automated-testing/main/tool-guidance/library/keycloak/sample.cypress.env.json' >> $testpath/sample.cypress.env.json
     mkdir $testpath/cypress/e2e/examples/keycloak-example
     curl 'https://raw.githubusercontent.com/bcgov/automated-testing/main/tool-guidance/library/keycloak/example/keycloak-example.cy.js' >> $testpath/cypress/e2e/examples/keycloak-example/keycloak-example.cy.$code
